5c188c45c61e9b97a7b68d3f79cfcbb54609b956
Andrei <andreirech22@outlook.com>
2024-09-11 16:06:57 -0300

    Modificando função

--- app/services/csv_service.py
+++ app/services/csv_service.py
@@ -1,27 +1,46 @@
-from fastapi import HTTPException, UploadFile
+import csv
+from fastapi import HTTPException, Path
+from sqlmodel import Session
+from app.models.game import Game
 
-from app.utils.file_handler import read_csv_file, save_file
-
-
-async def process_csv(file: UploadFile):
+def insert_csv_data(file_path: Path, session: Session) -> None:
     """
-    Processa o arquivo, valida se não está vazio e retorna as linhas
+    Lê dados de um arquivo CSV e insere no banco de dados.
     """
-    file_path = await save_file(file)
-
-    try:
-        rows = read_csv_file(file_path)
-
-        if len(rows) == 0:
-            raise HTTPException(
-                status_code=422, detail="CSV inválido. O arquivo precisa ter conteúdo."
+    data = read_csv_file(file_path)
+    
+    games = []
+    for row in data:
+        try:
+            game = Game(
+                name=row['Name'],
+                release_date=row['Release date'],
+                estimated_owners=row['Estimated owners'],
+                price=float(row['Price']),
+                about=row.get('About the game'),
+                metacritic_score=int(row['Metacritic score']),
+                positive_rev=int(row['Positive']),
+                negative_rev=int(row['Negative']),
+                achievements=int(row['Achievements']),
+                average_playtime=int(row['Average playtime forever']),
+                categories=row['Categories'],
+                genres=row['Genres'],
+                tags=row['Tags']
             )
-            
-        print(rows)
-
-        return rows
-
-    except Exception as e:
-        raise HTTPException(
-            status_code=500, detail=f"Erro {str(e)} ao processar o CSV."
-        )
+            games.append(game)
+        except ValueError as e:
+            raise HTTPException(status_code=400, detail=f"Erro ao processar a linha: {row}. Erro: {e}")
+        except KeyError as e:
+            raise HTTPException(status_code=400, detail=f"Campo ausente: {e}")
+    
+    session.add_all(games)
+    session.commit()
+        
+def read_csv_file(file_path: Path):
+    """
+    Realiza a leitura do csv e retorna todas as linhas para serem adicionadas no banco de dados.
+    """
+    csv.field_size_limit(1000000) 
+    with open(file_path, mode="r", encoding="utf-8") as file:
+        reader = csv.DictReader(file)
+        return [row for row in reader]
