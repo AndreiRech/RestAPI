7d789ce5b1373d08c737a013f3324cdbc3b1c9f2
Andrei <andreirech22@outlook.com>
2024-09-21 20:28:20 -0300

    alterando logica das rotas

--- app/routers/games.py
+++ app/routers/games.py
@@ -1,14 +1,11 @@
 from typing import Annotated, List, Optional
 from fastapi import APIRouter, Depends, HTTPException, Path, Query
+from sqlalchemy import String, cast, func
 from app.models.game import Game, GameBase, GameUpdate
 from sqlmodel import Session, or_, select
 from app.database.db import get_session
 
 router = APIRouter(prefix="/games", tags=["Games"])
-
-@router.get('/')
-def front():
-   return{ 'message': 'Bem vindo a API de jogos'}
 
 @router.get('', response_model=List[Game], status_code=200)
 async def games(
@@ -20,12 +17,13 @@ async def games(
     - **session**: Dependência que fornece a sessão do banco de dados.
     
     Retorna uma lista de objetos `Game` representando os jogos.
+    
+    Não recomendada a utilização pois o tempo de espera é extremamente alto.
 
     - **Exemplo de Resposta:**
       ```json
       [
         {
-          "id": 1,
           "name": "Counter-Strike",
           "release_date": "Aug 20, 2024",
           "estimated_owners": "10 - 2000",
@@ -41,7 +39,6 @@ async def games(
           "tags": "multiplayer, tactical"
         },
         {
-          "id": 2,
           "name": "The Witcher 3",
           "release_date": "May 19, 2015",
           "estimated_owners": "5,000,000 - 10,000,000",
@@ -62,9 +59,9 @@ async def games(
     game_list = session.exec(select(Game)).all()
     return game_list
 
-@router.get('/{game_name}', response_model=Game, status_code=200)
+@router.get('/name/{game_name}', response_model=Game, status_code=200)
 async def game(
-    game_name: Annotated[int, Path(title='O Nome do jogo')],
+    game_name: Annotated[str, Path(title='O Nome do jogo')],
     session: Session = Depends(get_session)
 ) -> Game:
     """
@@ -79,26 +76,26 @@ async def game(
 
     - **Exemplo de Requisição:**
       ```http
-      GET /games/The Witcher 3
+      GET /games/Cuphead
       ```
 
     - **Exemplo de Resposta:**
       ```json
       {
-        "id": 1,
-        "name": "The Witcher 3",
-        "release_date": "Aug 20, 2024",
-        "estimated_owners": "10 - 2000",
-        "price": 59.99,
-        "about": "Jogagando com o bruxão",
-        "metacritic_score": 85,
-        "positive_rev": 15000,
-        "negative_rev": 500,
-        "achievements": 50,
-        "average_playtime": 120,
-        "categories": "RPG",
-        "genres": "Action",
-        "tags": "singleplayer, fantasy"
+        "about": "Cuphead is a classic run and gun action game heavily focused on boss battles. Inspired by cartoons of the 1930s, [...]",
+        "price": 19.99,
+        "positive_rev": 107896,
+        "achievements": 42,
+        "categories": "Single-player,Multi-player,Co-op,Shared/Split Screen Co-op,Shared/Split Screen,Steam Achievements,Full controller support,Steam Trading Cards,Steam Cloud,Remote Play on Phone,Remote Play on Tablet,Remote Play on TV,Remote Play Together",
+        "tags": "Difficult,Cartoon,Platformer,Co-op,Great Soundtrack,Local Co-Op,2D,Hand-drawn,Multiplayer,Indie,Retro,Cartoony,Bullet Hell,Action,Shoot 'Em Up,Side Scroller,Singleplayer,Colorful,Funny,Atmospheric",
+        "id": 3812,
+        "estimated_owners": "2000000 - 5000000",
+        "name": "Cuphead",
+        "release_date": "Sep 29, 2017",
+        "metacritic_score": 88,
+        "negative_rev": 3869,
+        "average_playtime": 763,
+        "genres": "Action,Indie"
       }
       ```
 
@@ -106,13 +103,21 @@ async def game(
       - `200 OK`: O jogo foi encontrado e a resposta contém os dados do jogo.
       - `404 Not Found`: O jogo com o ID fornecido não foi encontrado.
     """
-    game = session.get(Game, game_name)
-    if game is None:
-        raise HTTPException(status_code=404, detail='Jogo não encontrado')
     
-    return game
+    clean_game_name = game_name.replace(" ", "").lower()
+
+    query = select(Game).where(func.lower(func.replace(Game.name, " ", "")) == clean_game_name)
+    result = session.exec(query).first()
+    
+    print(f"Comparando com CLEAN_NAME: {clean_game_name}")
+    print(f"Comparando com RESULT : {result}")
+    
+    if result is None:
+      raise HTTPException(status_code=404, detail='Jogo não encontrado')
+    
+    return result
   
-@router.get('/{game_id}', response_model=Game, status_code=200)
+@router.get('/id/{game_id}', response_model=Game, status_code=200)
 async def game(
     game_id: Annotated[int, Path(title='O ID do jogo')],
     session: Session = Depends(get_session)
@@ -129,26 +134,26 @@ async def game(
 
     - **Exemplo de Requisição:**
       ```http
-      GET /games/1
+      GET /games/2264
       ```
 
     - **Exemplo de Resposta:**
       ```json
       {
-        "id": 1,
-        "name": "Counter-Strike",
-        "release_date": "Aug 20, 2024",
-        "estimated_owners": "10 - 2000",
-        "price": 59.99,
-        "about": "A popular tactical shooter game.",
-        "metacritic_score": 85,
-        "positive_rev": 15000,
-        "negative_rev": 500,
-        "achievements": 50,
-        "average_playtime": 120,
-        "categories": "Shooter",
-        "genres": "Action",
-        "tags": "multiplayer, tactical"
+        "about": "Game of the Year EDITION The Witcher 3: Wild Hunt Game of the Year edition [...]",
+        "price": 0,
+        "positive_rev": 0,
+        "achievements": 78,
+        "categories": "Single-player,Steam Achievements,Full controller support,Steam Trading Cards,Steam Cloud",
+        "tags": "",
+        "id": 2264,
+        "estimated_owners": "0 - 0",
+        "name": "The Witcher 3: Wild Hunt - Game of the Year Edition",
+        "release_date": "Aug 29, 2016",
+        "metacritic_score": 0,
+        "negative_rev": 0,
+        "average_playtime": "0",
+        "genres": "RPG"
       }
       ```
 
@@ -192,27 +197,74 @@ async def search(
         - `200 OK`: A solicitação foi bem-sucedida e a resposta contém a lista de jogos.
         
     - **Exemplo de Requisição:**
-        - URL: `/search/?q=Counter:Strike`
+        - URL: `/search/?q=Counter-Strike:`
         - Descrição: Busca jogos que contenham "Counter-Strike" em qualquer um dos campos pesquisados.
 
     - **Exemplo de Resposta:**
       ```json
       [
         {
-          "id": 1,
-          "name": "Counter-Strike",
-          "release_date": "Nov 8, 2000",
-          "estimated_owners": "5,000,000 - 10,000,000",
-          "price": 19.99,
-          "about": "A popular tactical shooter game.",
-          "metacritic_score": 85,
-          "positive_rev": 15000,
-          "negative_rev": 500,
-          "achievements": 50,
-          "average_playtime": 120,
-          "categories": "Shooter",
-          "genres": "Action",
-          "tags": "multiplayer, tactical"
+          "about": "With its extensive Tour of Duty campaign, a near-limitless number of skirmish modes, [...]",
+          "price": 9.99,
+          "positive_rev": 19314,
+          "achievements": 0,
+          "categories": "Single-player,Multi-player,Valve Anti-Cheat enabled",
+          "tags": "Action,FPS,Shooter,Multiplayer,First-Person,Singleplayer,Classic,Tactical,[...]",
+          "id": 9908,
+          "estimated_owners": "5000000 - 10000000",
+          "name": "Counter-Strike: Condition Zero",
+          "release_date": "Mar 1, 2004",
+          "metacritic_score": 65,
+          "negative_rev": 1871,
+          "average_playtime": 1523,
+          "genres": "Action"
+        },
+        {
+          "about": "THE NEXT INSTALLMENT OF THE WORLD'S # 1 ONLINE ACTION GAME Counter-Strike: Source blends [...]",
+          "price": 9.99,
+          "positive_rev": 135151,
+          "achievements": 147,
+          "categories": "Multi-player,Cross-Platform Multiplayer,Steam Achievements,[...]",
+          "tags": "Shooter,Action,FPS,Multiplayer,Team-Based,First-Person,Tactical,[...]",
+          "id": 21611,
+          "estimated_owners": "10000000 - 20000000",
+          "name": "Counter-Strike: Source",
+          "release_date": "Nov 1, 2004",
+          "metacritic_score": 88,
+          "negative_rev": 5348,
+          "average_playtime": 9171,
+          "genres": "Action"
+        },
+        {
+          "about": "With its extensive Tour of Duty campaign, a near-limitless [...]",
+          "price": 9.99,
+          "positive_rev": 13442,
+          "achievements": 0,
+          "categories": "Single-player,Multi-player,Valve Anti-Cheat enabled",
+          "tags": "Action,FPS,Shooter,Multiplayer,First-Person,Classic,Singleplayer, [...]",
+          "id": 40951,
+          "estimated_owners": "10000000 - 20000000",
+          "name": "Counter-Strike: Condition Zero",
+          "release_date": "Mar 1, 2004",
+          "metacritic_score": 65,
+          "negative_rev": 1535,
+          "average_playtime": 1321,
+          "genres": "Action"
+        },
+        {
+          "about": "Counter-Strike: Global Offensive (CS: GO) expands upon the team-based action gameplay [...]'",
+          "price": 0,
+          "positive_rev": 5764420,
+          "achievements": 167,
+          "categories": "Multi-player,Steam Achievements,Full controller support,Steam Trading Cards,[...]",
+          "id": 46159,
+          "estimated_owners": "50000000 - 100000000",
+          "name": "Counter-Strike: Global Offensive",
+          "release_date": "Aug 21, 2012",
+          "metacritic_score": 83,
+          "negative_rev": 766677,
+          "average_playtime": 30484,
+          "genres": "Action,Free to Play"
         }
       ]
       ```
@@ -220,17 +272,19 @@ async def search(
     stmt = select(Game)
     
     if q:
-        q = q.lower()
-        stmt = stmt.where(
-            or_(
-                Game.name.ilike(f'%{q}%'),
-                Game.price.ilike(f'%{q}%'),
-                Game.metacritic_score.ilike(f'%{q}%'),
-                Game.categories.ilike(f'%{q}%'),
-                Game.genres.ilike(f'%{q}%'),
-                Game.tags.ilike(f'%{q}%')
-            )
-        )
+      q = q.strip().lower()
+        
+      search_fields = [
+        Game.name.ilike(f'%{q}%'),
+        cast(Game.price, String).ilike(f'%{q}%'),
+        cast(Game.metacritic_score, String).ilike(f'%{q}%'),
+        Game.categories.ilike(f'%{q}%') if Game.categories is not None else None,
+        Game.genres.ilike(f'%{q}%') if Game.genres is not None else None,
+        Game.tags.ilike(f'%{q}%') if Game.tags is not None else None
+      ]
+        
+      search_conditions = [field for field in search_fields if field is not None]
+      stmt = stmt.where(or_(*search_conditions))
     
     game_list = session.exec(stmt).all()
     return game_list
@@ -272,20 +326,20 @@ async def create(
     - **Exemplo de Resposta:**
       ```json
       {
-        "id": 1,
+        "about": "A popular tactical shooter game.",
+        "price": 59.99,
+        "positive_rev": 15000,
+        "achievements": 50,
+        "categories": "Shooter",
+        "tags": "multiplayer, tactical",
+        "id": 1334103,
+        "estimated_owners": "10 - 2000",
         "name": "Counter-Strike",
         "release_date": "Aug 20, 2024",
-        "estimated_owners": "10 - 2000",
-        "price": 59.99,
-        "about": "A popular tactical shooter game.",
         "metacritic_score": 85,
-        "positive_rev": 15000,
         "negative_rev": 500,
-        "achievements": 50,
-        "average_playtime": 120,
-        "categories": "Shooter",
-        "genres": "Action",
-        "tags": "multiplayer, tactical"
+        "average_playtime": "120",
+        "genres": "Action"
       }
       ```
     """
@@ -301,7 +355,7 @@ async def create(
 async def delete(
   game_id: Annotated[int, Path(title='O ID do jogo a ser deletado')],
   session: Session = Depends(get_session)
-) -> str:
+) -> dict:
   """
   Deleta um jogo com base no ID fornecido.
   
@@ -315,12 +369,28 @@ async def delete(
 
   - **Exemplo de Requisição:**
     ```http
-    DELETE /games/1
+    DELETE /games/2264
     ```
 
   - **Exemplo de Resposta:**
     ```json
-    "Jogo deletado com sucesso."
+      "message": "Jogo deletado com sucesso.",
+      "game": {
+        "about": "Game of the Year EDITION The Witcher 3: Wild Hunt Game of the Year edition [...]",
+        "price": 0,
+        "positive_rev": 0,
+        "achievements": 78,
+        "categories": "Single-player,Steam Achievements,Full controller support,Steam Trading Cards,Steam Cloud",
+        "tags": "",
+        "id": 2264,
+        "estimated_owners": "0 - 0",
+        "name": "The Witcher 3: Wild Hunt - Game of the Year Edition",
+        "release_date": "Aug 29, 2016",
+        "metacritic_score": 0,
+        "negative_rev": 0,
+        "average_playtime": "0",
+        "genres": "RPG"
+      }
     ```
 
   - **Código de Status:**
@@ -356,9 +426,10 @@ async def edit(
 
     - **Exemplo de Requisição:**
       ```json
+      "id": 46160
       {
-        "title": "Counter-Strike: Global Offensive",
-        "description": "A tactical shooter game",
+        "name": "Counter-Strike: Global Offensive",
+        "about": "Jogo de dar bala",
         "release_date": "Aug 21, 2024"
       }
       ```
@@ -368,10 +439,20 @@ async def edit(
       {
         "message": "Jogo editado com sucesso",
         "game": {
-          "id": 1,
-          "title": "Counter-Strike: Global Offensive",
-          "description": "A tactical shooter game",
-          "release_date": "Aug 21, 2024"
+          "about": "Jogo de dar bala",
+          "price": 8.99,
+          "positive_rev": 0,
+          "achievements": 6,
+          "categories": "Multi-player,PvP,Online PvP,Steam Achievements,Full controller support,Stats",
+          "tags": "Action,Casual,Flight,Multiplayer,Arcade,Character Customization,3D,Colorful,PvP,Side Scroller,Combat",
+          "id": 46160,
+          "estimated_owners": "0 - 20000",
+          "name": "Counter-Strike: Global Offensive",
+          "release_date": "May 31, 2021",
+          "metacritic_score": 0,
+          "negative_rev": 2,
+          "average_playtime": 0,
+          "genres": "Action,Casual"
         }
       }
       ```
