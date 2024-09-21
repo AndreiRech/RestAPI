d4e5b052b49c9b57454642506c1383adf067c648
Andrei <andreirech22@outlook.com>
2024-09-21 20:28:01 -0300

    iniciando informacoes ao iniciar

--- app/database/db.py
+++ app/database/db.py
@@ -10,8 +10,8 @@ engine = create_engine(DATABASE_URL, echo=True)
 
 def init_db():
     SQLModel.metadata.create_all(engine)
-##    with Session(engine) as session:
-##        insert_csv_data(CSV_DIR, session)
+    with Session(engine) as session:
+        insert_csv_data(CSV_DIR, session)
     
 def get_session():
     with Session(engine) as session:
