52d0cd1e0476ea8561717f18e0eea1a229d9d22a
Andrei <andreirech22@outlook.com>
2024-09-22 09:25:22 -0300

    oops

--- app/routers/welcome.py
+++ app/routers/welcome.py
@@ -4,7 +4,7 @@ from sqlmodel import Session
 from app.database.db import get_session
 
 
-router = APIRouter(prefix="/", tags=["Welcome"])
+router = APIRouter(prefix="", tags=["Welcome"])
 
 @router.get('', status_code=200)
 async def welcome(session: Session = Depends(get_session)):
