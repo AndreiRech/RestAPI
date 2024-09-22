da4933e8dd7db9cdc848ce409da484667694d6ad
Andrei <andreirech22@outlook.com>
2024-09-22 09:31:31 -0300

    voltando com alterações de rotas

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from app.database.db import init_db
 
-from app.routers import games, welcome
+from app.routers import games
 
 app = FastAPI()
 
@@ -11,6 +11,5 @@ def init(app: FastAPI) -> None:
     """
     init_db()
     app.include_router(games.router)
-    app.include_router(welcome.router)
     
 init(app)
